59ede41a2cbcdfd7e4e240dd17a54f4b14a00f38
Lawani Imrane <48188711+naimlawani01@users.noreply.github.com>
2023-07-09 14:36:36 +0200

    Schemas dto

---
+++ schemas/schemas_dto.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+from pydantic import BaseModel
+# DTO : Data Transfert Object
+
+
+
+class Camera_POST_Body (BaseModel):
+    brand : str
+    model : str
+    resolution: str
+    price: float
+    description: str
+    availability: bool
+    rating: float
+
+
+class Camera_PATCH_Body (BaseModel):
+    price: float
+    description: str
+    availability: bool
+    rating: float
+
+class Customer_POST_Body (BaseModel):
+    customerEmail:str
+    customerPassword: str
+
+class Customer_response (BaseModel):
+    id: int
+    email:str
+    create_at: datetime
+    class Config: # Importante pour la traduction ORM->DTO
+        orm_mode= True
+        
+        
+class OrderBase(BaseModel):
+    camera_id: int
+    quantity: int
+    total_amount: float
+    shipping_address: str
+    status: str
+
+class OrderCreate(OrderBase):
+    pass
+
+class Order(OrderBase):
+    class Config:
+        orm_mode = True
+
+class ReviewBase(BaseModel):
+    camera_id: int
+    review_text: str
+
+class ReviewCreate(ReviewBase):
+    pass
+
+class Review(ReviewBase):
+
+    class Config:
+        orm_mode = True
